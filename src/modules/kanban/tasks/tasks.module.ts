8cb1083c5d67d24daa4d774acbe1cc9da0e00c27
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2026-01-14 23:07:37 -0300

    feat: verify if users instances in 'create()' task are members of board

--- src/modules/kanban/tasks/tasks.module.ts
+++ src/modules/kanban/tasks/tasks.module.ts
@@ -3,9 +3,11 @@ import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './entities/task.entity';
+import { BoardColumnsModule } from '../board-columns/board-columns.module';
+import { BoardModule } from '../board/board.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Task]), BoardColumnsModule, BoardModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
--- src/modules/kanban/tasks/tasks.service.ts
+++ src/modules/kanban/tasks/tasks.service.ts
@@ -1,19 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { Repository } from 'typeorm';
 import { Task } from './entities/task.entity';
 import { UserRequestDto } from 'src/modules/auth/dto/user-request.dto';
 import { InjectRepository } from '@nestjs/typeorm';
+import { BoardColumnsService } from '../board-columns/board-columns.service';
+import { BoardService } from '../board/board.service';
 
 @Injectable()
 export class TasksService {
   constructor(
     @InjectRepository(Task)
     private readonly repository: Repository<Task>,
+
+    private readonly boardColumnsService: BoardColumnsService,
+    private readonly boardService: BoardService,
   ) {}
 
-  create({ user, payload }: { user: UserRequestDto; payload: CreateTaskDto }) {
+  async create({
+    user,
+    payload,
+  }: {
+    user: UserRequestDto;
+    payload: CreateTaskDto;
+  }) {
+    const board = await this.boardColumnsService.getBoardByColumnId({
+      id: payload.columnId,
+    });
+
+    const creatorIsMember = await this.boardService.isMember({
+      userId: user.id,
+      boardId: board.id,
+    });
+
+    if (!creatorIsMember) {
+      throw new UnprocessableEntityException(
+        'Creator must be a member of the board',
+      );
+    }
+
+    const responsibleIsMember = await this.boardService.isMember({
+      boardId: board.id,
+      userId: payload.responsibleId,
+    });
+
+    if (!responsibleIsMember) {
+      throw new UnprocessableEntityException(
+        'Assignee must be a member of the board',
+      );
+    }
+
     const task = this.repository.create({
       ...payload,
       creator: {

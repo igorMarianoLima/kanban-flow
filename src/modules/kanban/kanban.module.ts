6a60273b67cd13f2abbdf19c8c3c1351e6a9ca23
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2026-01-18 13:26:33 -0300

    feat: create initial 'Invites' module and resources

--- src/modules/kanban/kanban.module.ts
+++ src/modules/kanban/kanban.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { BoardModule } from './board/board.module';
 import { TasksModule } from './tasks/tasks.module';
 import { BoardColumnsModule } from './board-columns/board-columns.module';
+import { InvitesModule } from './invites/invites.module';
 
 @Module({
-  imports: [BoardModule, TasksModule, BoardColumnsModule],
+  imports: [BoardModule, TasksModule, BoardColumnsModule, InvitesModule],
 })
 export class KanbanModule {}

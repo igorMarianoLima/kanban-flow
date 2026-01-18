abe1aab4015ea1480b5a43812322903759972499
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2026-01-17 23:10:35 -0300

    feat: dinamically provide mailer adapter in 'Email' module

--- src/modules/email/email.module.ts
+++ src/modules/email/email.module.ts
@@ -1,7 +1,37 @@
 import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
+import { EmailAdapterContract } from './contracts/email-adapter.contract';
+import { ConfigService } from '../config/config.service';
+import { NodemailerServiceAdapter } from './adapters/nodemailer-service.adapter';
+import { createTransport } from 'nodemailer';
 
 @Module({
-  providers: [EmailService]
+  providers: [
+    EmailService,
+    {
+      provide: EmailAdapterContract,
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const config = configService.getEmailConfig();
+
+        if (config.provider === 'nodemailer') {
+          const transport = createTransport({
+            host: config.host,
+            port: config.port,
+            secure: config.ssl,
+            ...(config.auth.user && {
+              auth: {
+                user: config.auth.user,
+                pass: config.auth.pass,
+              },
+            }),
+          });
+
+          return new NodemailerServiceAdapter(transport, configService);
+        }
+      },
+    },
+  ],
+  exports: [EmailService],
 })
 export class EmailModule {}

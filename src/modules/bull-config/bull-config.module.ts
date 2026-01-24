0d2fbcb484c904c9b7414e22f0131f5cb09fe4aa
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2026-01-24 19:18:23 -0300

    feat: implement redis connection in 'BullConfig' module

--- src/modules/bull-config/bull-config.module.ts
+++ src/modules/bull-config/bull-config.module.ts
@@ -1,4 +1,25 @@
+import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
+import { ConfigService } from '../config/config.service';
+import { ConfigModule } from '../config/config.module';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule,
+    BullModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const redisConf = configService.getRedisConfig();
+
+        return {
+          connection: {
+            host: redisConf.host,
+            portt: redisConf.port,
+          },
+        };
+      },
+    }),
+  ],
+  exports: [BullModule],
+})
 export class BullConfigModule {}

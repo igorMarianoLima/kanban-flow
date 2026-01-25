0564cceec34bf3b600078299b0b2f6f0d4d27b6b
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2026-01-24 22:43:37 -0300

    feat: use email processor in 'worker'

--- src/worker/worker.module.ts
+++ src/worker/worker.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { BullConfigModule } from 'src/modules/bull-config/bull-config.module';
+import { EmailQueueProcessorModule } from 'src/queues/email/processor/email-queue-processor.module';
 
 @Module({
-  imports: [BullConfigModule],
+  imports: [BullConfigModule, EmailQueueProcessorModule],
 })
 export class WorkerModule {}
